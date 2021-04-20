84e6be3240dbf1251e8572bc4231bed079b32e94
佘桢澄 <>
2021-04-20 19:06:10 +0800

    ✨ 特性: 初始化仓库&增加简单组件

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Component } from 'react'
+import Hello from './components/Hello.js'
+
+export default class Demo extends Component {
+  render () {
+    return (
+      <div>
+        <Hello />
+      </div>
+    )
+  }
+}
---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+
+export default class Hello extends Component {
+  render () {
+    return (
+      <div>
+        <h2>
+          你好呀
+        </h2>
+      </div>
+    )
+  }
+}
