d483a309cf15102c19a776848dba8a4b5ebc4c03
佘桢澄 <>
2021-04-21 11:37:14 +0800

    ✨ 特性: 组件放到文件夹中及后缀改为jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Component } from 'react'
-import Hello from './components/Hello.js'
+import Hello from './components/Hello'
+import Welcome from './components/Welcome'
 
 export default class Demo extends Component {
   render () {
     return (
       <div>
         <Hello />
+        <Welcome />
       </div>
     )
   }
---
+++ src/components/Hello/index.jsx
@@ -0,0 +1,10 @@
+import { Component } from 'react'
+import './index.css'
+
+export default class Hello extends Component {
+  render () {
+    return (
+      <h1 className="hello">这是hello组件</h1>
+    )
+  }
+}
