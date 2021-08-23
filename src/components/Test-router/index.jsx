f751720ffd9c5446bac62fd8cdcb7fcaec284bd3
佘桢澄 <601956669@qq.com>
2021-08-23 22:28:29 +0800

    ✨ 特性: link修改为navlink，设置点击完成后的css

--- src/components/Test-router/index.jsx
+++ src/components/Test-router/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link,Route,BrowserRouter } from 'react-router-dom'
+import { NavLink,Route,BrowserRouter } from 'react-router-dom'
 import A from './a'
 import B from './b'
 
@@ -10,8 +10,8 @@ export default class test extends Component {
         {/* 路由类的必须放到同一个BrowserRouter中才能生效*/}
         <BrowserRouter>
           {/* 控制路由变化 */}
-          <Link to="/a">toA</Link>
-          <Link to="/b">toB</Link>
+          <NavLink activeClassName="demo" to="/a">toA</NavLink>
+          <NavLink activeClassName="demo" to="/b">toB</NavLink>
 
           {/* 注册组件，监测路由变化显示不同的组件 */}
           <Route path="/a" component={A} />
